adf7579091335c386995fa7dabd3e0750f57068f
rwonyujin03-debug <rwonyujin03@gmail.com>
2026-07-02 13:43:36 +0900

    Add CORS support to the Express server

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const port = 3000;
+
+let cors0ptions = {
+  origin: "*",
+};
+
+app.use(cors(cors0ptions));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
