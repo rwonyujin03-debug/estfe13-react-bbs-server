39fad41a5ea7185e8bb1276633e4bd7aefdc6b90
rwonyujin03-debug <rwonyujin03@gmail.com>
2026-07-07 11:00:21 +0900

    Implement CRUD operations and MySQL integration in Express server

--- index.js
+++ index.js
@@ -1,16 +1,106 @@
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
 const app = express();
+const mysql = require("mysql2");
 const port = 3000;
+const multer = require("multer");
 
-let cors0ptions = {
+app.use(express.json()); //json->object
+app.use(express.urlencoded({ extended: true })); //html form ->object
+
+let corsOptions = {
   origin: "*",
 };
 
-app.use(cors(cors0ptions));
+app.use(cors(corsOptions));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniquePrefix + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345",
+  database: "bbs",
+});
+
+db.connect();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/list", (req, res) => {
+  const sqlQuery = "SELECT id, title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board;";
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.get("/view", (req, res) => {
+  console.log(req.query.id);
+  const id = req.query.id;
+  // const sqlQuery = `SELECT * FROM board WHERE id=${req.query.id};`;
+  const sqlQuery = "SELECT title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board WHERE id=?;";
+  db.query(sqlQuery, [id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/write", upload.single("image"), (req, res) => {
+  console.log(req.body);
+  const { title, writer, content } = req.body;
+  const imagePath = req.file ? req.file.path : null; //req.file.path는 업로드된 파일의 경로
+
+  const sqlQuery = "insert into board (title,content,writer,image_path) values (?,?,?,?);";
+  db.query(sqlQuery, [title, content, writer, imagePath], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/delete", (req, res) => {
+  console.log(req.body);
+  const { id } = req.body;
+
+  const sqlQuery = "DELETE FROM board WHERE id=?";
+  db.query(sqlQuery, [id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/deleteselect", (req, res) => {
+  console.log(req.body);
+  const { boardIdList } = req.body;
+
+  const sqlQuery = `delete from board where id in (${boardIdList})`;
+  db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/update", (req, res) => {
+  console.log(req.body);
+  const { name, title, content, id } = req.body;
+
+  const sqlQuery = "UPDATE board SET writer=?, title=?, content=? WHERE id=?";
+  db.query(sqlQuery, [name, title, content, id], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 app.listen(port, () => {
